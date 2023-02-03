32a566556879bcc21a2c65baef373a0001cb20a8
eduardocoria31099 <eduardo.coria@altatec.com.mx>
2023-02-02 18:16:36 -0600

    feat(app)add age

--- app/src/main/java/com/example/onboarding/ui/adapter/PersonAdapter.kt
+++ app/src/main/java/com/example/onboarding/ui/adapter/PersonAdapter.kt
@@ -2,14 +2,18 @@ package com.example.onboarding.ui.adapter
 
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.onboarding.R
 import com.example.onboarding.databinding.ItemListPeopleBinding
 import com.example.onboarding.model.PersonEntity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PersonAdapter(
     private var person: List<PersonEntity>,
@@ -24,6 +28,7 @@ class PersonAdapter(
 
     override fun getItemCount(): Int = person.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
         holder.bind(person[position])
 
@@ -33,10 +38,22 @@ class PersonAdapter(
         private val onClick: (PersonEntity) -> Unit,
     ) : RecyclerView.ViewHolder(view) {
         private val binding = ItemListPeopleBinding.bind(view)
+
+        @SuppressLint("SimpleDateFormat")
+        @RequiresApi(Build.VERSION_CODES.O)
         fun bind(person: PersonEntity) = with(binding) {
             tvName.text = person.name
 
-            tvAge.text = person.birthday
+            val dateBirthday: Date? = SimpleDateFormat("yyyy-MM-dd").parse(person.birthday ?: "")
+            val dayActual = Date(System.currentTimeMillis())
+            val differences = dayActual.time - dateBirthday!!.time
+            val seconds = differences / 1000
+            val minutes = seconds / 60
+            val horas = minutes / 60
+            val dias = horas / 24
+            val years = dias / 365
+
+            tvAge.text = years.toString()
             val imageBytes = Base64.decode(person.img, 0)
             val image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
             imgPeople.setImageBitmap(image)
--- app/src/main/java/com/example/onboarding/ui/dlg/DialogPerson.kt
+++ app/src/main/java/com/example/onboarding/ui/dlg/DialogPerson.kt
@@ -1,10 +1,13 @@
 package com.example.onboarding.ui.dlg
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import com.example.onboarding.databinding.DialogPersonBinding
 import com.example.onboarding.model.PersonEntity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DialogPerson(context: Context, personEntity: PersonEntity) {
     private var binding: DialogPersonBinding
@@ -26,13 +29,26 @@ class DialogPerson(context: Context, personEntity: PersonEntity) {
         setListeners()
     }
 
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
     private fun setListeners() {
         binding.apply {
             tvName.text = person.name
-            tvAge.text = person.birthday
+            val dateBirthday: Date? = SimpleDateFormat("yyyy-MM-dd").parse(person.birthday ?: "")
+            val dayActual = Date(System.currentTimeMillis())
+            val differences = dayActual.time - dateBirthday!!.time
+            val seconds = differences / 1000
+            val minutes = seconds / 60
+            val horas = minutes / 60
+            val dias = horas / 24
+            val years = dias / 365
+            tvAge.text = years.toString()
             tvAddress.text = person.address
             tvNumber.text = person.number
-            tvHobbies.text = person.hobbies ?: "No hobbies"
+            if (person.hobbies!!.isEmpty()) {
+                tvHobbies.text = "No hobbies"
+            } else {
+                tvHobbies.text = person.hobbies ?: "No hobbies"
+            }
             imgExit.setOnClickListener {
                 if (onOptionSelectedListener != null) {
                     onOptionSelectedListener!!.onCancelOptionSelected()
